8e485290006254627eb7dd1c5c56771951e18964
AdePrasTyo <adeprasestyo6@gmail.com>
2024-07-02 18:23:45 +0700

    Update file env

---
+++ storage/framework/views/47526ab74ce859096d51061da7cda626.php
@@ -0,0 +1,100 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        body {
+            background-image: url("images/Prestigue-car-wash.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+
+        }
+
+        .centered-text {
+            margin: 0% auto;
+            text-align: center;
+            border: 1px solid rgb(2, 10, 133);
+            background-color: rgb(0, 4, 255);
+            opacity: 0.6;
+            max-width: 50%;
+            color: aliceblue;
+        }
+
+        button {
+            margin-left: 300px;
+            margin-top: 300px;
+
+            text-align: center;
+            border: 1px solid rgba(133, 2, 2, 0.301);
+            background-color: rgb(255, 0, 0);
+            color: aliceblue;
+            font-size: 40px;
+
+        }
+    </style>
+</head>
+
+
+<body>
+    <h1 class="centered-text" align="center">
+        Selamat datang di Sistem Manajemen Cuci Mobil
+    </h1>
+    <button>
+        <?php if (isset($component)) { $__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.link','data' => ['href' => '/ticket/create']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('link'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['href' => '/ticket/create']); ?>
+            Buat tiket
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b)): ?>
+<?php $attributes = $__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b; ?>
+<?php unset($__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b)): ?>
+<?php $component = $__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b; ?>
+<?php unset($__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b); ?>
+<?php endif; ?>
+    </button>
+    <button>
+        <?php if (isset($component)) { $__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.link','data' => ['href' => '/tracking']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('link'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['href' => '/tracking']); ?>
+            Lacak tiket Anda
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b)): ?>
+<?php $attributes = $__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b; ?>
+<?php unset($__attributesOriginal90eee3f94ef0e1b15e49c277c8700e9b); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b)): ?>
+<?php $component = $__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b; ?>
+<?php unset($__componentOriginal90eee3f94ef0e1b15e49c277c8700e9b); ?>
+<?php endif; ?>
+    </button>
+</body>
+
+
+</html>
+<?php /**PATH C:\Primary Storage\Documents\Git\iWash\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f012cf2b08243926b8b51e05c35fe2bf.php
@@ -0,0 +1,5 @@
+<a href="<?php echo e($href); ?>" class="<?php echo e($class ?? ''); ?>">
+    <?php echo e($slot); ?>
+
+</a>
+<?php /**PATH C:\Primary Storage\Documents\Git\iWash\resources\views/components/link.blade.php ENDPATH**/ ?>
